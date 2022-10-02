8e49fbac761628854bf7c00d480378a45d6c8e42
jharinkesh <rinkeshjha7@gmail.com>
2022-10-02 21:33:11 +0530

    BST-search

--- dsa/src/main/java/tree/BST.java
+++ dsa/src/main/java/tree/BST.java
@@ -16,7 +16,32 @@ public class BST {
         root = insert(root, 2);
         root = insert(root, 5);
         root = insert(root, 1);
-        inorder(root);
+        root = insertRecurive(root, 20);
+//        inorder(root);
+
+//        System.out.println(search(root, 51));
+        System.out.println(max(root));
+    }
+
+    static int max(Node root){
+        int max = root.key;
+        while(root.right !=null){
+            max = root.key;
+            root = root.right;
+        }
+        max = root.key;
+        return max;
+    }
+
+    static boolean search(Node root, int k){
+        if(root == null)
+            return false;
+        if(k == root.key)
+            return true;
+        if(k<root.key)
+            return search(root.left, k);
+        else
+            return search(root.right, k);
     }
 
     static void inorder(Node root){
@@ -25,6 +50,16 @@ public class BST {
             System.out.print(root.key+ " ");
             inorder(root.right);
         }
+    }
+
+    static Node insertRecurive(Node root, int key){
+        if(root == null)
+            return new Node(key);
+        if(key<root.key)
+            root.left = insertRecurive(root.left, key);
+        else
+            root.right = insertRecurive(root.right, key);
+        return root;
     }
 
     static Node insert(Node root, int key){
