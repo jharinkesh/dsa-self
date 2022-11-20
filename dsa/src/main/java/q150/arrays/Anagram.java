b75c6431664533fa127e92f326c8dab8ab053e14
jharinkesh <rinkeshjha7@gmail.com>
2022-11-20 20:48:01 +0530

    graoup anagram

--- dsa/src/main/java/q150/arrays/Anagram.java
+++ dsa/src/main/java/q150/arrays/Anagram.java
@@ -1,12 +1,39 @@
 package main.java.q150.arrays;
 
+import java.util.HashMap;
+import java.util.*;
+
 public class Anagram {
     public static void main(String[] args) {
-        System.out.println(isAna("ca","rac"));
-        System.out.println(isAna("malayalam","amlayalam"));
-        System.out.println(isAna("malayalam","malayamam"));
+//        System.out.println(isAna("ca","rac"));
+//        System.out.println(isAna("malayalam","amlayalam"));
+//        System.out.println(isAna("malayalam","malayamam"));
+        groupAnagram(new String[]{"aba","cbc","aab"});
     }
 
+
+    static void groupAnagram(String[] strs){
+        Map<String, List> m = new HashMap<>();
+        for(String s:strs){
+            Integer[] k = new Integer[26];
+            Arrays.fill(k,0);
+            for(int j=0;j<s.length();j++)
+                k[s.charAt(j)-97] += 1;
+
+            String K = Arrays.toString(k);
+            if(m.containsKey(K)){
+                m.get(K).add(s);
+            }else{
+                List l = new ArrayList();
+                l.add(s);
+                m.put(K, l);
+            }
+        }
+//        System.out.println(m);
+        for(List ls: m.values()){
+            System.out.print(ls +" ");
+        }
+    }
 
     static boolean isAna(String a, String b){
         if(a.length()!=b.length())
